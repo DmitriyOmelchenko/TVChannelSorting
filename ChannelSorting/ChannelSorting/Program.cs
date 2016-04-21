5d5e39d7bef85c0cc95a374041be3a845be70979
DmitriyOmelchenko <d.omelchenko@pervolo.com>
2016-04-21 16:54:08 +0300

    channel sorting

---
+++ ChannelSorting/ChannelSorting/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ChannelSorting
+{
+    class Program
+    {
+        static void GetAndSortChannels(string path)
+        {
+            if (!File.Exists(path))
+                return;
+            List<TVChannel> channels = new List<TVChannel>();
+            var channelsNameAndCountry = File.ReadAllLines(path).Where(
+                (l, index) =>l.Length>0 && index % 2 == 0).ToArray();
+            var channelsUrl= File.ReadAllLines(path).Where(
+                (l, index) => l.Length > 0 && index % 2!= 0).ToArray();
+            for (int i = 0; i < channelsUrl.Length; i++)
+            {
+                var value = channelsNameAndCountry[i].Split(':');
+                var country = value.Length > 2?  value[1]:"Default";
+                var name = value.Length > 2 ? value[2] : value[1];
+                var url=new string(channelsUrl[i].Select(c => c).Skip(channelsUrl[i].IndexOf(':') + 1).ToArray());
+
+                channels.Add(new TVChannel(country, name, url));
+            }
+          
+            using (StreamWriter sr=File.CreateText("SortByCountryChannels.txt"))
+            {
+                foreach (var sortChannels in channels.GroupBy(c => c.Country))
+                {
+                    sr.WriteLine(sortChannels.Key+":");
+                    foreach (var channel in sortChannels)
+                    {
+                        sr.WriteLine(channel.Name);
+                        sr.WriteLine(channel.URL);
+                    }
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            GetAndSortChannels("webtv list (2).txt");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ChannelSorting/ChannelSorting/TVChannel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChannelSorting
+{
+    class TVChannel
+    {
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+
+        public TVChannel(string country,string name,string url)
+        {
+            Country = country;
+            Name = name;
+            URL = url;
+
+        }
+     
+    }
+}
